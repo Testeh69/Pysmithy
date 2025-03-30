96e926339eff1b3dd1ca3ad6acb4f0ae39551a26
Nicolas Orefice <norefice45@gmail.com>
2025-03-30 17:08:54 +0200

    graph travel using q learning algorithm

---
+++ py_impl/model/graph.py
@@ -0,0 +1,88 @@
+from typing import Union
+import numpy as np
+import random
+
+
+
+graphCities = {
+    "Paris": ["Lyon", "Marseille", "Toulouse", "Bordeaux"],
+    "Lyon": ["Paris", "Marseille", "Grenoble", "Nice"],
+    "Marseille": ["Paris", "Lyon", "Nice", "Toulouse"],
+    "Toulouse": ["Paris", "Marseille", "Bordeaux", "Lille"],
+    "Bordeaux": ["Paris", "Toulouse", "Lille"],
+    "Grenoble": ["Lyon"],
+    "Nice": ["Lyon", "Marseille"],
+    "Lille": ["Paris", "Bordeaux"]
+}
+
+
+
+
+
+class QLearningGraphTravel:
+
+
+
+    def __init__(self, graphcities, epsilon = 0.8, value_init = 0):
+        self.graph_cities = graphcities
+        self.Q_table = {city: {action: value_init for action in graphcities[city]} for city in graphcities}
+        self.travel = []
+        self.history_epsilon = epsilon
+        self.epsilon = epsilon
+        self.state = None
+        self.next_best_action = None
+    
+
+    def train(self,beginning,end, epochs = 10, limit = 1000, lr = 0.001,gamma = 0.9, reward = -6, verbose = True):
+        for x in range(epochs):
+            self.epsilon = self.history_epsilon
+            self.state = beginning
+            nb_travel_cities = 0
+            total_reward = 0
+            step_reward = 0
+            while self.state != end and nb_travel_cities < limit:
+                self.travel.append(self.state)
+                self.choiceAction()
+                step_reward = 0.1 if self.state == end else reward
+                max_next_Q = max(self.Q_table[self.next_best_action].values()) if self.Q_table[self.next_best_action] else 0
+                self.Q_table[self.state][self.next_best_action] += lr*(step_reward + gamma* max_next_Q - self.Q_table[self.state][self.next_best_action])
+                self.state = self.next_best_action
+                total_reward += step_reward
+                nb_travel_cities +=1
+                if self.state not in self.graph_cities.keys():
+                    break
+
+        
+            
+            if verbose:
+                print(f"Epoch {x + 1}/{epochs} - Total Rewards: {total_reward}")
+                print(f"Path: {self.travel}")     
+                
+
+    def test(self,beginning,end, max_iterations = 100):
+        
+        self.state = beginning
+        iter = 0
+        self.travel = [beginning]
+        while self.state != end and iter < max_iterations:
+            if self.state == end:
+                print(f"Arrivé : {self.state}/{end}")
+            self.state = max(self.Q_table[self.state], key=self.Q_table[self.state].get)
+            self.travel.append(self.state)
+
+            iter += 1
+        return self.travel
+
+    def choiceAction(self, decay =  0.001):
+        self.epsilon = max(0.1, self.epsilon * np.exp(-decay))         
+        x = np.random.uniform(0,1)
+        if x < self.epsilon:
+            self.next_best_action = random.choice(self.graph_cities[self.state])
+        else:
+            self.next_best_action = max(self.Q_table[self.state], key=self.Q_table[self.state].get)
+        return self.next_best_action
+
+
+
+
+
--- smithy/src/model/regression.rs
+++ smithy/src/model/regression.rs
@@ -9,36 +9,3 @@ use ndarray_rand::RandomExt;
 
 
 
-#[pyclass]
-pub struct linear_regression {
-    pub methode_resolution:i32,
-    pub weight:Option<Array2<f64>>,
-}
-
-
-
-impl linear_regression{
-
-    pub fn new(resolution:i32)-> linear_regression{
-        linear_regression{
-            methode_resolution: resolution,
-            weight:None,
-        }
-    }
-
-    pub fn fit(&mut self, x_train:&Array2<f32>, y_train:&Array2<f32>, learning_rate:f32, epochs:usize)-> Array2<f64>{
-        let (n_samples,n_featues) = x_train.dim();
-        let mut weight  =  Array2::random((n_featues,1), Uniform::new(0.0, 1.0));
-        let mut result = x_train.t().dot(x_train);
-        if (self.methode_resolution == -1){
-            weight = result.pinv().dot(x_train.t().dot(y_train))
-        }
-        self.weight = weight;
-        return self.weight;
-    }
-
-    pub fn predict(&self,x_test:&Array2){
-        return x_test.dot(self.weight);
-    }
-
-}
